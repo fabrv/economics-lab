a2d9de82adb02af0d665d7aa9f4a46fd1406da90
alejclemus <alejdelcomp@gmail.com>
2019-04-21 23:36:54 -0600

    MVP html

--- server.js
+++ server.js
@@ -22,32 +22,9 @@ server.listen(5000, function() {
 });
 
 var players = {};
-io.on('connection', function(socket) {
-  socket.on('new player', function() {
-    players[socket.id] = {
-      x: 300,
-      y: 300
-    };
-  });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.up) {
-      player.y -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.down) {
-      player.y += 5;
-    }
-  });
+io.sockets.on('connect', function(socket) {
+  var sessionid = socket.id;
 });
-setInterval(function() {
-  io.sockets.emit('state', players);
-}, 1000 / 60);
 
 
 // Insert username & password into 'user'
--- static/game.js
+++ static/game.js
@@ -1,62 +1,16 @@
 var socket = io();
-var movement = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
-}
-document.addEventListener('keydown', function(event) {
-    switch (event.keyCode) {
-        case 65: // A
-            movement.left = true;
-            break;
-        case 87: // W
-            movement.up = true;
-            break;
-        case 68: // D
-            movement.right = true;
-            break;
-        case 83: // S
-            movement.down = true;
-            break;
-    }
-});
-document.addEventListener('keyup', function(event) {
-    switch (event.keyCode) {
-        case 65: // A
-            movement.left = false;
-            break;
-        case 87: // W
-            movement.up = false;
-            break;
-        case 68: // D
-            movement.right = false;
-            break;
-        case 83: // S
-            movement.down = false;
-            break;
-    }
+let socket = io.connect('http://localhost:<portNumber>');
+console.log(socket.id); // undefined
+socket.on('connect', () => {
+    console.log(socket.id); // an alphanumeric id...
 });
 
-socket.emit('new player');
-setInterval(function() {
-    socket.emit('movement', movement);
-}, 1000 / 60);
-
-var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
-var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-    context.clearRect(0, 0, 800, 600);
-    context.fillStyle = 'green';
-    for (var id in players) {
-        var player = players[id];
-        context.beginPath();
-        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-        context.fill();
-    }
+var input1 = new CanvasInput({
+    canvas: document.getElementById('accionesempresaB')
 });
+
+
+
 
 // Server side input handler, modifies the state of the players and the
 // game based on the input it receives. Everything runs asynchronously with
@@ -89,3 +43,4 @@ socket.on('disconnect', function() {
         isNotification: true
     });
 });
+
